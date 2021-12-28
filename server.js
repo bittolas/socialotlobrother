fb797d2668805ce916d00c2154bda398a96d19de
bittolas <80890990+bittolas@users.noreply.github.com>
2021-12-28 02:42:36 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+var onoof = "1";
+var HOST = location.origin.replace(/^http/, 'ws')
+      var ws = new WebSocket('wss://socialotlo.herokuapp.com/');
+      var el;
+      ws.onopen = (event) => {
+      console.log('conectado')
+      };
+      ws.onclose = () => {
+        switch(onoof) {
+           case '1': {  
+        
+          console.log('conexao perdida')
+           setTimeout(
+                 function() {
+                          HOST = location.origin.replace(/^http/, 'ws')
+                          ws = new WebSocket("wss://otloserver.herokuapp.com/");
+                          
+                     console.log(" a tentar reconectar...")
+                     conetador();
+                  }, 15000);
+          } break;
+        }
+      }
